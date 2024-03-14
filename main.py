b7afc72a4d1397690f47cd266fdec6a54d7a1aa1
Shashank Bhandari <114545974+Shashank11203@users.noreply.github.com>
2024-03-14 15:25:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
+import base64
+
+
 
 
 load_dotenv()
+import os
 
-#google_api_key = os.getenv("GOOGLE_API_KEY")
 
 
-os.environ['GOOGLE_API_KEY'] = 'YOUR_API_KEY'
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+if google_api_key is None:
+raise ValueError("Google API key not found. Make sure it's set in the .env file.")
 
-# Your code here
-
+#To get the text from the pdf
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
@@ -29,19 +34,19 @@ def get_pdf_text(pdf_docs):
     return  text
 
 
-
+#To get the chunks of the text
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
-
+#To coonvert the text chunks into embeddings and store it in DB
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
-
+#Conversational Chain 
 def get_conversational_chain():
 
     prompt_template = """
@@ -66,7 +71,7 @@ def get_conversational_chain():
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
@@ -77,21 +82,54 @@ def user_input(user_question):
         , return_only_outputs=True)
 
     print(response)
-    st.write("Reply: ", response["output_text"])
-
-
+    return response
 
 
 def main():
-    st.set_page_config("PDF Querying")
-    st.header("Chat with PDF using Gemin AI ")
-    with st.container():
-     user_question = st.text_input("User Input:", key="user_input")
-    #user_question = st.chat_input("Ask a Question from the PDF Files")
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+  
+    st.markdown(
+        """
+        <style>
+         .st-emotion-cache-1c7y2kd{
+         flex-direction :row-reverse;
+         text-align: right;
+             }
+        </style>
+        """,
+    unsafe_allow_html=True,
+    )
+    
+    if "messages" not in st.session_state.keys():
+      st.session_state.messages = [
+    {"role": "assistant", "content": "Ask a Question from the PDF Files"}]
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-    if user_question:
-        user_input(user_question)
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user",avatar="🤵"):
+            st.write(prompt)
+           
+    # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
 
+       
     with st.sidebar:
         st.title("Menu:")
         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
@@ -101,7 +139,7 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
                 st.success("Done")
-    
+
 
 
 if __name__ == "__main__":
